6715c11be3ad6b17762f79cc9dd92782ecebc7d2
Unknown <franco.labra@usach.cl>
2018-08-13 12:03:55 -0300

    scripts agregados
    con un insert

---
+++ database/scripts/script.sql
@@ -0,0 +1,260 @@
+/* Script SQL ProyectoDBD */
+
+/*--------------------------------------------------------*/
+
+/* Tabla proveedorAutos */
+
+CREATE TABLE proveedorAutos(
+	id int NOT NULL AUTO INCREMENT,
+	nombreProveedorAuto varchar(50) NOT NULL,
+	telefono varchar(15) NOT NULL,
+	correo varchar(30) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla proveedorTraslados */
+
+CREATE TABLE proveedorTraslados(
+	id int NOT NULL AUTO INCREMENT,
+	nombreProveedorTraslado varchar(50) NOT NULL,
+	telefono varchar(15) NOT NULL,
+	correo varchar(30) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla paquetes */
+
+CREATE TABLE paquetes(
+	id int NOT NULL AUTO INCREMENT,
+	precioDescuento int NOT NULL,
+	tipoPaquete int NOT NULL,
+	precioNormal int NOT NULL,
+	descripcion varchar(150) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla aerolineas */
+
+CREATE TABLE aerolineas(
+	id int NOT NULL AUTO INCREMENT,
+	nombreAerolinea varchar(50) NOT NULL,
+	tipoAerolinea int NOT NULL,
+	calificacion int,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla ubicaciones */
+
+CREATE TABLE ubicaciones(
+	id int NOT NULL AUTO INCREMENT,
+	latitud varchar(15) NOT NULL,
+	longitud varchar(15) NOT NULL,
+	cuidad varchar(100) NOT NULL,
+	pais varchar(100) NOT NULL,
+	calleUbicacion varchar(50) NOT NULL,
+	numeroUbicacion int NOT NULL,
+	codigoPostal int NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla historialusers */
+
+CREATE TABLE historialusers(
+	id int NOT NULL AUTO INCREMENT,
+	descripcion varchar(150) NOT NULL,
+	fechaHistorial date NOT NULL,
+	horaHistorial time NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla users */
+
+CREATE TABLE users(
+	id int NOT NULL AUTO INCREMENT,
+	historialuser_id int NOT NULL,
+	name varchar(30) NOT NULL,
+	email varchar(50) NOT NULL,
+	password varchar(10) NOT NULL,
+	dni_Pasaporte int NOT NULL,
+	fondos int NOT NULL,
+	rol int NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla compras */
+
+CREATE TABLE compras(
+	id int NOT NULL AUTO INCREMENT,
+	user int NOT NULL,
+	descripcion varchar(150) NOT NULL,
+	monto int NOT NULL,
+	fechaCompra date NOT NULL,
+	horaCompra time NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla autos */
+
+CREATE TABLE autos(
+	id int NOT NULL AUTO INCREMENT,
+	compra_id int NOT NULL,
+	proveedor_auto_id int NOT NULL,
+	patente varchar(10) NOT NULL,
+	precio int NOT NULL,
+	marca varchar(15) NOT NULL,
+	modelo varchar(20) NOT NULL,
+	fechaArriendo date NOT NULL,
+	horaArriendo time NOT NULL,
+	fechaDevolucion date NOT NULL,
+	horaDevolucion time NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla actividads */
+
+CREATE TABLE actividads(
+	id int NOT NULL AUTO INCREMENT,
+	compra_id int NOT NULL,
+	nombreActividad varchar(50),
+	precioActividad int NOT NULL,
+	descripcion varchar(250) NOT NULL,
+	numeroNinos int NOT NULL,
+	numeroAdultos int NOT NULL,
+	fechaInicio date NOT NULL,
+	horaInicio time NOT NULL,
+	fechaTermino date NOT NULL,
+	horaTermino time NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla historialCompras */
+
+CREATE TABLE historialCompras(
+	id int NOT NULL AUTO INCREMENT,
+	compra_id int NOT NULL,
+	fechaCompra date NOT NULL,
+	horaCompra time NOT NULL,
+	tipoCompra int NOT NULL,
+	metodoDePago int NOT NULL,
+	monto int NOT NULL,
+	descripcion varchar(200), NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla alojamientos */
+
+CREATE TABLE alojamientos(
+	id int NOT NULL AUTO INCREMENT,
+	ubicacione_id int NOT NULL,
+	nombreAlojamiento varchar(20),
+	calificacion int, 
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla habitaciones */
+
+CREATE TABLE habitaciones(
+	id int NOT NULL AUTO INCREMENT,
+	compra_id int NOT NULL,
+	alojamiento_id int NOT NULL,
+	precioHabitacion int NOT NULL,
+	calificacion int,
+	numeroAdultos int NOT NULL,
+	numeroNinos int NOT NULL,
+	tipoHabitacion int NOT NULL,
+	fechaIngreso date NOT NULL,
+	horaIngreso time NOT NULL,
+	fechaSalida date NOT NULL,
+	horaSalida time NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla traslados */
+
+CREATE TABLE traslados(
+	id int NOT NULL AUTO INCREMENT,
+	compra_id int NOT NULL,
+	preoveedor_traslado_id int NOT NULL,
+	precioTraslado int NOT NULL,
+	planificacionParadas varchar(200) NOT NULL,
+	numeroParadas int NOT NULL,
+	totalPasajeros int NOT NULL,
+	fechaTraslado date NOT NULL,
+	horaTraslado time NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla aeropuertos */
+
+CREATE TABLE aeropuertos(
+	id int NOT NULL AUTO INCREMENT,
+	ubicacione_id int NOT NULL,
+	nombreAeropuerto varchar(50) NOT NULL,
+	tipoAeropuerto int NOT NULL,
+	calificacion int,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla vuelos */
+
+CREATE TABLE vuelos(
+	id int NOT NULL AUTO INCREMENT,
+	compra_id int NOT NULL,
+	aerolinea_id int NOT NULL,
+	aeropuertoOrigen_id int NOT NULL,
+	aeropuertoDestino_id int NOT NULL,
+	tipoVuelo int NOT NULL,
+	precioVuelo int NOT NULL,
+	numeroEscala int NOT NULL,
+	cantidadEquipaje int NOT NULL,
+	fechaPartida date NOT NULL,
+	horaPartida time NOT NULL,
+	fechaRegreso date NOT NULL,
+	horaRegreso time NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
+
+/* Tabla asientos */
+
+CREATE TABLE asientos(
+	id int NOT NULL AUTO INCREMENT,
+	vuelo_id int NOT NULL,
+	disponibilidad boolean NOT NULL,
+	numeroAsiento int NOT NULL,
+	tipoAsiento int NOT NULL,
+	PRIMARY KEY (id)
+);
+
+/*--------------------------------------------------------*/
